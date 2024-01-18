6f43b8c418657d39d53314cc2d68105eaee553a9
gabriel-cm-saldanha <gabriel.cmsaldanha@gmail.com>
2024-01-18 14:37:10 -0300

    versao 2.0

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, shuffle
+import pyperclip
+import json
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
@@ -16,6 +18,7 @@ def generate_password():
 
     password = "".join(password_list)
     password_entry.insert(0, password)
+    pyperclip.copy(password)
 
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
@@ -25,17 +28,31 @@ def save_data():
     login = login_entry.get()
     password = password_entry.get()
 
+    new_data = {
+        website:{
+            "login":login,
+            "password":password
+            }
+    }
+
     if len(website.strip()) == 0 or len(password.strip()) == 0:
         messagebox.showinfo(title="Ooopsss", message="Sorry But you missed a blank field")
     else:
         is_ok = messagebox.askokcancel(title=website,
                                        message=f"These are the details entered: \nLogin: {login}\n password: {password}\n Is it ok to save?")
         if is_ok:
-            with open("data.txt", "a") as file:
-                file.write(f"\n{website} | {login} | {password}")
+            with open("data.json", "w") as data_file:
+                json.dump(new_data, data_file, indent=4)
             website_entry.delete(0, END)
             password_entry.delete(0, END)
 
+# --------------------------- SEARCH DATA ----------------------------- #
+def search_info():
+    website = website_entry.get()
+    with open('data.json','r') as data_file:
+        data = json.load(data_file)
+        for site in data:
+            print(data[site])
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -52,7 +69,7 @@ canvas.grid(column=1, row=0)
 website_label = Label(text="Website:", font=("Arial", 10, "bold"))
 website_label.grid(column=0, row=1)
 
-website_entry = Entry(width=45)
+website_entry = Entry(width=20)
 website_entry.grid(column=1, row=1, columnspan=2, sticky='EW')
 website_entry.focus()
 
@@ -75,6 +92,9 @@ password_entry.grid(column=1, row=3, sticky='EW')
 
 
 # Buttons
+search_button = Button(text="Search", width=15, command=search_info)
+search_button.grid(column=2, row=1)
+
 generate_button = Button(text="Generate Password", command=generate_password)
 generate_button.grid(column=2, row=3)
 
